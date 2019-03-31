a36a4aeaf3fa81bd6f9eb1ea9ae9c31b254c3dd1
jakeogh <github.com@v6y.net>
2019-03-31 00:52:58 -0700

    initial commit

---
+++ newapp/newapp.py
@@ -0,0 +1,351 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+import click
+from kcl.dirops import dir_exists
+from kcl.dirops import create_dir
+
+@click.command()
+@click.argument('apppath')
+@click.option('--verbose', is_flag=True)
+def newapp(apppath, verbose):
+    appname = os.path.filename(apppath)
+    app_collection_folder = os.path.dirname(apppath)
+    eprint("creating app:", appname)
+    eprint("in:", app_collection_folder)
+    assert not dir_exists(apppath)
+    create_dir(apppath)
+
+if __name__ == '__main__':
+    newapp()
+
+#import os
+#import sys
+#
+## drop into shell anywhere with:
+## import IPython; IPython.embed()
+## import pdb; pdb.set_trace()
+## from pudb import set_trace; set_trace(paused=False)
+## https://news.ycombinator.com/item?id=16611827 3.7
+#
+##from pudb.remote import set_trace
+##set_trace(term_size=(80, 24))
+#
+#
+##def log_uncaught_exceptions(ex_cls, ex, tb):
+##   cprint(''.join(traceback.format_tb(tb)))
+##   cprint('{0}: {1}'.format(ex_cls, ex))
+##
+##sys.excepthook = log_uncaught_exceptions
+#
+##https://github.com/Lukasa/utils/blob/master/two_digit_numbers.py
+#
+#def main(path):
+#    print(path)
+#    with open(path, 'rb') as fh:
+#        data = fh.read()
+#
+## handle stdin or a filename as input
+#if __name__ == '__main__':
+#    import sys
+#    if len(sys.argv[:]) < 2:
+#        path = '/dev/stdin'
+#        main(path)
+#    else:
+#        for path in sys.argv[1:]:
+#            if path.startswith(b'file://'):
+#                main(path)
+#            else:
+#                main(path)
+#
+#import os
+#home = os.path.expanduser("~")
+#os.path.sep = b'/'
+#os.path.altsep = b'/'
+#program_folder = os.path.dirname(os.path.realpath(__file__))
+#
+#if len(sys.argv[]) < 2:
+#        path = '/dev/stdin'
+#else:
+#        path = sys.argv[1]
+#
+#input_fh = open(path, 'rb')
+#try:
+#    input = sys.argv[1]
+#except:
+#    input = open('/dev/stdin', 'r').read()
+#
+#
+#try:
+#    filedir = os.path.expanduser(sys.argv[1])
+#except IndexError:
+#    print 'Script must be called with one argument.'
+#    sys.exit(1)
+#
+#if (not filedir[-1] == '/'):
+#    filedir += '/'
+#files = os.listdir(filedir)
+#
+#
+#
+##standard example:
+#def main(dsn):
+#    "Do something"
+#    print("ok")
+#
+#if __name__ == '__main__':
+#    import sys
+#    n = len(sys.argv[1:])
+#    if n == 0:
+#        sys.exit('usage: python %s dsn' % sys.argv[0])
+#    elif n == 1:
+#        main(sys.argv[1])
+#    else:
+#        sys.exit('Unrecognized arguments: %s' % ' '.join(sys.argv[2:]))
+#
+#
+##argparse example:
+#def main(dsn):
+#    "Do something"
+#    print(dsn)
+#
+#if __name__ == '__main__':
+#    import argparse
+#    p = argparse.ArgumentParser()
+#    p.add_argument('dsn')
+#    arg = p.parse_args()
+#    main(arg.dsn)
+#
+#
+##plac example (uses argparse): -h and --help work
+#def main(dsn):
+#    "Do something"
+#    print(dsn)
+#
+#if __name__ == '__main__':
+#    import plac
+#    plac.call(main)
+#
+#
+#def debug(func):
+#    msg = func.__qualname__
+#    @wraps(func)
+#    #http://www.dabeaz.com/py3meta/Py3Meta.pdf
+#    def wrapper(*args, **kwargs):
+#        print(msg)
+#        return func(*args, **kwargs)
+#    return wrapper
+#
+#
+##http://liw.fi/cmdtest/
+##http://liw.fi/cliapp/
+#
+#"""
+#SYNOPSIS
+#
+#    TODO helloworld [-h] [-v,--verbose] [--version]
+#
+#DESCRIPTION
+#
+#    TODO This describes how to use this script.
+#    This docstring will be printed by the script if there is an error or
+#    if the user requests help (-h or --help).
+#
+#EXAMPLES
+#
+#    TODO: Show some examples of how to use this script.
+#
+#EXIT STATUS
+#
+#    TODO: List exit codes
+#
+#AUTHOR
+#
+#    TODO: Name <name@example.org>
+#
+#LICENSE
+#
+#    TODO: This script is not licensed yet.
+#
+#VERSION
+#
+#"""
+#
+##import urllib
+##import urlparse
+##import serial
+##import numpy
+##import scipy
+#
+#import sys
+#import os
+#import traceback
+#import optparse
+#import time
+##from pexpect import run, spawn
+#from os.path import exists
+#from sys import exit
+#
+## Uncomment the following section if you want readline history support.
+##import readline, atexit
+##histfile = os.path.join(os.environ['HOME'], '.TODO_history')
+##try:
+##    readline.read_history_file(histfile)
+##except IOError:
+##    pass
+##atexit.register(readline.write_history_file, histfile)
+#
+#def main ():
+#
+#    global options, args
+#
+#        try:
+#        print "options=",options
+#        print "args=",args
+#
+#        except IndexError:
+#                sys.stderr.write(helpstring)
+#
+#
+#if __name__ == '__main__':
+#    try:
+#            start_time = time.time()
+#            parser = optparse.OptionParser(formatter=optparse.TitledHelpFormatter(), usage=globals()['__doc__'], version='0.1')
+#            parser.add_option('-v', '--verbose', action='store_true', default=False, help='verbose output')
+#            parser.add_option('-q', '--quick', action='store_true', default=False, help='quick compare, not 100%')
+#            (options, args) = parser.parse_args()
+#            if len(args) < 1:
+#                parser.error('missing argument')
+#            if options.verbose: print time.asctime()
+#
+#            exit_code = main()
+#
+#            if exit_code is None:
+#                    exit_code = 0
+#            if options.verbose: print time.asctime()
+#            if options.verbose: print 'TOTAL TIME IN MINUTES:',
+#            if options.verbose: print (time.time() - start_time) / 60.0
+#        print " "
+#            sys.exit(exit_code)
+#        except KeyboardInterrupt, e: # Ctrl-C
+#            raise e
+#        except SystemExit, e: # sys.exit()
+#            raise e
+#        except Exception, e:
+#            print 'ERROR, UNEXPECTED EXCEPTION'
+#            print str(e)
+#            traceback.print_exc()
+#            os._exit(1)
+#
+#
+#def formatExceptionInfo(maxTBlevel=5):
+#    cla, exc, trbk = sys.exc_info()
+#    excName = cla.__name__
+#    try:
+#        excArgs = exc.__dict__["args"]
+#    except KeyError:
+#        excArgs = "<no args>"
+#
+#    excArgsString = ''
+#    for item in excArgs:
+#        excArgsString = excArgsString + ' ' + str(item)
+#
+#    excTb = traceback.format_tb(trbk, maxTBlevel)
+#    excTbString = ''
+#    for item in excTb:
+#        excTbString = excTbString + " " + str(item)
+#
+#    report = "%s %s %s"%(excName, excArgsString, excTbString)
+#    return(report)
+#
+#
+#
+##formatting strings
+#
+#
+#
+##http://stackoverflow.com/questions/1549509/remove-duplicates-in-a-list-while-keeping-its-order-python
+#def unique(seq):
+#    seen = set()
+#    for item in seq:
+#        if item not in seen:
+#            seen.add(item)
+#            yield item
+#
+#
+#
+#def exec_command(command):
+#    tar_cmd_proc = subprocess.Popen(tar_command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+#    cprint("executing tar_command:", tar_command)
+#    tar_cmd_proc_output_stdout, tar_cmd_proc_output_stderr = tar_cmd_proc.communicate(gpg_cmd_proc_output_stdout)
+#    cprint("tar_cmd_proc_output_stdout:")
+#    tar_cmd_proc_output_stdout_decoded = tar_cmd_proc_output_stdout.decode('utf-8')
+#    for line in tar_cmd_proc_output_stdout_decoded.split('\n'):
+#        cprint("STDOUT:", line)
+#
+#    cprint("tar_cmd_proc_output_stderr:")
+#    tar_cmd_proc_output_stderr_decoded = tar_cmd_proc_output_stderr.decode('utf-8')
+#    for line in tar_cmd_proc_output_stderr_decoded.split('\n'):
+#        cprint("STDERR:", line)
+#
+#    cprint("tar_cmd_proc.returncode:", tar_cmd_proc.returncode)
+#
+#    if tar_cmd_proc.returncode != 0:
+#        cprint("tar did not return 0")
+#        return False
+#
+#
+#def reverse_sort_list(domains):
+#    data = []
+#    for x in domains:
+#        d = x.strip()[::-1]
+##        print("d:", d)
+#        data.append(d)
+#    data.sort() #sorting a list of strings by tld
+#    for y in data:
+##        print("y:", y)
+#        y = y[::-1]
+#        print(y)
+#
+#
+#def domain_set_to_sorted_list_grouped_by_tld(domains):
+#    data = []
+#    for x in domains:
+#       d = x.strip().split('.')
+#       d.reverse()
+#       data.append(d)
+#    data.sort()
+#    for y in data:
+#       y.reverse()
+#       print('.'.join(y))
+#
+#
+#
+#def print_hex(text):
+#    print(':'.join(hex(ord(x))[2:] for x in text))
+#
+#
+#
+#def dprint(*args, **kwargs):
+#    if click_debug:
+#        caller = sys._getframe(1).f_code.co_name
+#        print(str("%.5f" % time.time()), os.getpid(), '{0: <15}'.format(caller+'()'), *args, file=sys.stderr, **kwargs)
+#
+#def eprint(*args, **kwargs):
+#    print(*args, file=sys.stderr, **kwargs)
+#
+#print(pydoc.render_doc(logger))
+#
+#log_level=log_levels['DEBUG']
+#log_level=log_levels['INFO:']
+#
+## pylint: disable=invalid-name
+## pylint: enable=invalid-name
+#
+#
+#def print_traceback():
+#    ex_type, ex, tb = sys.exc_info()
+#    traceback.print_tb(tb)
+#    del tb
+#
+#
