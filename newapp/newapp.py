df8a9be3fd251259a07dc312c120ce4f3573995d
Justin Keogh <github.com@v6y.net>
2019-06-14 14:08:12 -0700

    auto-commit

--- newapp/newapp.py
+++ newapp/newapp.py
@@ -21,7 +21,8 @@ def cli():
 @click.pass_context
 def new(ctx, new_apppath, git_repo, verbose):
     if not (new_apppath or git_repo):
-        click.echo(ctx.parent.get_help(), color=ctx.color)
+        #click.echo(ctx.parent.get_help(), color=ctx.color)
+        click.echo(ctx.get_help(), color=ctx.color)
     if new_apppath:
         apppath = os.path.realpath(os.path.expanduser(new_apppath))
         app_collection_folder, appname = os.path.split(apppath)
