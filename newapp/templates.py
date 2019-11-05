f97e0cffb90784e739ae5eae7339327d506c4b9b
jakeogh <github.com@v6y.net>
2019-11-04 18:34:27 -0700

    auto-commit

--- newapp/templates.py
+++ newapp/templates.py
@@ -45,7 +45,7 @@ ebuild = '''# Copyright 1999-2019 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
-PYTHON_COMPAT=( python{{3_6,3_7}} )
+PYTHON_COMPAT=( python3_{{6,7}} )
 
 inherit distutils-r1 git-r3
 
