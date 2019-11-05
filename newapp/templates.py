586cb89c55d3da7300f1d35dd79e8faeb9fd19bb
jakeogh <github.com@v6y.net>
2019-11-04 17:56:57 -0700

    auto-commit

--- newapp/templates.py
+++ newapp/templates.py
@@ -91,7 +91,7 @@ import sys
 import fastentrypoints
 from setuptools import find_packages, setup
 if not sys.version_info[0] == 3:
-    sys.exit("Python 3 is required. Use: \'python3 setup.py install\'")
+    sys.exit("Python 3 is required. Use: \\'python3 setup.py install\\'")
 
 dependencies = ["icecream", "click", "colorama"]
 
