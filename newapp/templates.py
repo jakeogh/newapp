42afa97730a6a14de09c22ccd38d773c51faec11
jakeogh <github.com@v6y.net>
2020-10-08 03:30:41 -0700

    auto-commit

--- newapp/templates.py
+++ newapp/templates.py
@@ -30,6 +30,7 @@ app = '''#!/usr/bin/env python3
 # pylint: disable=R0903  # too few public methods
 # pylint: disable=E1101  # no member for base
 # pylint: disable=W0201  # attribute defined outside __init__
+# pylint: disable=R0916  # Too many boolean expressions in if statement
 ## pylint: disable=W0703  # catching too general exception
 
 import os
