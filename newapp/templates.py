3f9c7647d9ec4989c38dc5eaebef235390af7753
jakeogh <github.com@v6y.net>
2021-01-26 15:50:59 -0700

    auto-commit

--- newapp/templates.py
+++ newapp/templates.py
@@ -221,7 +221,7 @@ def cli(ctx,
 
 '''
 
-ebuild = '''# Copyright 1999-2020 Gentoo Authors
+ebuild = '''# Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
