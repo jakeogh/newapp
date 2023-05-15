dd738240e8391037419284f1f25bcbfa44f80c53
jakeogh <github.com@v6y.net>
2023-05-15 14:55:47 -0700

    auto-commit

--- newapp/newapp.py
+++ newapp/newapp.py
@@ -1046,7 +1046,7 @@ def rename(
                 _err=sys.stderr,
             )
             new_ebuild_path = Path(new_app_name / new_ebuild_name).resolve()
-            sh.git.add("-u")
+            sh.git.add("*")
             sh.git.commit(
                 "-m",
                 "rename",
