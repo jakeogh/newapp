d94204ab4fdcb0776cc4a47cde1d7e45491e7f57
jakeogh <github.com@v6y.net>
2024-06-02 21:06:29 -0700

    auto-commit

--- newapp/newapp.py
+++ newapp/newapp.py
@@ -1354,8 +1354,10 @@ def list_all_ebuilds(
                 icp(_path)
                 assert Path(_path).exists()
 
-            repo_line_items = repo_line.split('EGIT_REPO_URI="'[1])
-            repo_line_items = repo_line_items.split('"'[0])
+            repo_line_items = repo_line.split('EGIT_REPO_URI="')[1]
+            icp(repo_line_items)
+            repo_line_items = repo_line_items.split('"')[0]
+            icp(repo_line_items)
             repo_line_items = repo_line_items.split(" ")
             icp(repo_line_items)
 
