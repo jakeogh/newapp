74be733cd0796c9db6af6f2ca2bb65e569373527
jakeogh <github.com@v6y.net>
2024-06-02 20:16:35 -0700

    auto-commit

--- newapp/newapp.py
+++ newapp/newapp.py
@@ -1288,7 +1288,7 @@ def list_all_ebuilds(
                     break
             icp(repo_line)
             if "myapps" not in repo_line:
-                assert repo_line.startswith("EGIT_REPO_URI=https://github.com/")
+                assert repo_line.startswith('EGIT_REPO_URI="https://github.com/')
                 assert repo_line.endswith(".git")
 
             # if not remote.startswith("git@github.com:"):
