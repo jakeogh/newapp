3360f736522be8a2365cfda21e7e961fabda18de
jakeogh <github.com@v6y.net>
2025-09-17 00:14:45 -0700

    auto-commit

--- newapp/newapp.py
+++ newapp/newapp.py
@@ -253,7 +253,6 @@ def get_url_for_overlay(
 
         repo_url = repo_url.split("(")[-1].split(")")[0]
         if repo_name == overlay:
-            # ic(repo_url)
             return repo_url
 
     raise ValueError(f"unknown repo {overlay}")
